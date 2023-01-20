975fa877abd7f1edbabcff4ffcbcfd55e3836a76
JONGHYUK LEE <43637704+Shurrr@users.noreply.github.com>
2023-01-21 00:43:55 +0900

    Update 킹， 퀸， 룩， 비숍， 나이트， 폰.java
    더 좋은 코드는 없을까..에 대한 노력

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -7,23 +7,20 @@ import java.util.StringTokenizer;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        int[] chess = new int[] {1, 1, 2, 2, 2, 8};
+        int[] foundChess = new int[6];
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(s);
-
-        int[] arr = new int[6];
-        for (int i=0 ; i<arr.length ; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0 ; i < foundChess.length ; i++) {
+            foundChess[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.print( 1 - arr[0] + " ");
-        System.out.print( 1 - arr[1] + " ");
-        System.out.print( 2 - arr[2] + " ");
-        System.out.print( 2 - arr[3] + " ");
-        System.out.print( 2 - arr[4] + " ");
-        System.out.print( 8 - arr[5] + " ");
-
+        for (int i = 0 ; i < chess.length; i++) {
+            System.out.print( chess[i] - foundChess[i] + " ");
+        }
     }
 
 }
