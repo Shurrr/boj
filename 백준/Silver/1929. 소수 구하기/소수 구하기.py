017a04770ec05ddda7fbe1e541d70398b3724e2f
JONGHYUK LEE <43637704+Shurrr@users.noreply.github.com>
2023-02-06 19:09:26 +0900

    [Silver III] Title: 소수 구하기, Time: 5460 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,16 @@
+M, N = map(int, input().split())
+
+for i in range(M, N+1) :
+
+    if i > 1 :
+    
+        check = True
+
+        for j in range(2, int(i ** 0.5) + 1 ) :
+            
+            if i % j == 0 :
+                check = False
+                break
+
+        if check == True :
+            print(i)
